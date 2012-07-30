f88e362c9e681c712b83c09b79f5691df0ed212b
Dave Eddy <dave@daveeddy.com>
2012-07-29 20:06:26 -0700

    Adding option

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-module.exports = function() {
-  process.stdout.write('\033[2J\033[0f');
+module.exports = function(clear) {
+  if (clear !== false) {
+    process.stdout.write('\033[2J');
+  }
+  process.stdout.write('\033[0f');
 };
